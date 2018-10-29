794403205e22e6d8016a121534f6508f5548385b
zhuzi0 <m15856407328@163.com>
2018-10-29 10:07:25 +0800

    首页完成

--- src/js/index.js
+++ src/js/index.js
@@ -54,3 +54,270 @@ function mendZero(n){
       })
       	
 });
+
+$.get("ajax/ajax2.txt",function(data){
+	 var  object = JSON.parse(data);
+	 var  arr1   =object.ws;
+	 $.each(arr1,function(i,v){
+	 	     $(` <li>
+                   	   <img src="${v.img}">
+                   	   <p>${v.show}</p>
+                   	   <div class="money">
+                   	   <span>${v.newmoney}</span>
+                   	   <s>${v.oldmoney}</s>
+                   	   </div>
+				   </li>`).appendTo("main .caw .title .title-r .zxyp ul");
+	 })
+
+})
+
+$.get("ajax/ajax2.txt",function(data){
+	 var  object = JSON.parse(data);
+	 var  arr2   =object.fkyy;
+	 $.each(arr2,function(i,v){
+	 	     $(` <li>
+                   	   <img src="${v.img}">
+                   	   <p>${v.show}</p>
+                   	   <div class="money">
+                   	   <span>${v.newmoney}</span>
+                   	   <s>${v.oldmoney}</s>
+                   	   </div>
+				   </li>`).appendTo("main .caw .title .title-r .fkyy ul");
+	 })
+
+})
+$.get("ajax/ajax2.txt",function(data){
+	 var  object = JSON.parse(data);
+	 var  arr3   =object.gmyy;
+	 $.each(arr3,function(i,v){
+	 	     $(` <li>
+                   	   <img src="${v.img}">
+                   	   <p>${v.show}</p>
+                   	   <div class="money">
+                   	   <span>${v.newmoney}</span>
+                   	   <s>${v.oldmoney}</s>
+                   	   </div>
+				   </li>`).appendTo("main .caw .title .title-r .gmyy ul");
+	 })
+
+})
+$.get("ajax/ajax2.txt",function(data){
+	 var  object = JSON.parse(data);
+	 var  arr4   =object.guke;
+	 $.each(arr4,function(i,v){
+	 	     $(` <li>
+                   	   <img src="${v.img}">
+                   	   <p>${v.show}</p>
+                   	   <div class="money">
+                   	   <span>${v.newmoney}</span>
+                   	   <s>${v.oldmoney}</s>
+                   	   </div>
+				   </li>`).appendTo("main .caw .title .title-r .guke ul");
+	 })
+
+})
+$.get("ajax/ajax2.txt",function(data){
+	 var  object = JSON.parse(data);
+	 var  arr5   =object.pifu;
+	 $.each(arr5,function(i,v){
+	 	     $(` <li>
+                   	   <img src="${v.img}">
+                   	   <p>${v.show}</p>
+                   	   <div class="money">
+                   	   <span>${v.newmoney}</span>
+                   	   <s>${v.oldmoney}</s>
+                   	   </div>
+				   </li>`).appendTo("main .caw .title .title-r .pifu ul");
+	 })
+
+})
+$.get("ajax/ajax2.txt",function(data){
+	 var  object = JSON.parse(data);
+	 var  arr6   =object.changwei;
+	 $.each(arr6,function(i,v){
+	 	     $(` <li>
+                   	   <img src="${v.img}">
+                   	   <p>${v.show}</p>
+                   	   <div class="money">
+                   	   <span>${v.newmoney}</span>
+                   	   <s>${v.oldmoney}</s>
+                   	   </div>
+				   </li>`).appendTo("main .caw .title .title-r .changwei ul");
+	 })
+
+})
+$.get("ajax/ajax2.txt",function(data){
+	 var  object = JSON.parse(data);
+	 var  arr7   =object.wuguan;
+	 $.each(arr7,function(i,v){
+	 	     $(` <li>
+                   	   <img src="${v.img}">
+                   	   <p>${v.show}</p>
+                   	   <div class="money">
+                   	   <span>${v.newmoney}</span>
+                   	   <s>${v.oldmoney}</s>
+                   	   </div>
+				   </li>`).appendTo("main .caw .title .title-r .wuguan ul");
+	 })
+
+})
+for(var m = 0 ; m < 6 ; m++){
+		$("main .caw").eq(m).find(" .title .title-r a").eq(0).hover(function(){
+			$(this).find(".zxyp").css('display',"block");
+			$(this).addClass("te");
+			$(this).siblings().removeClass("te");
+			$(this).siblings().find(".fkyy , .gmyy ,.guke, .pifu , .changwei ,.wuguan ").css("display","none");
+		})
+		$("main .caw").eq(m).find(" .title .title-r a").eq(1).hover(function(){
+			$(this).find(".fkyy").css('display',"block");
+			$(this).addClass("te");
+			$(this).siblings().removeClass("te");
+			$(this).siblings().find(".zxyp , .gmyy ,.guke, .pifu , .changwei ,.wuguan ").css("display","none");
+		})
+		$("main .caw").eq(m).find(" .title .title-r a").eq(2).hover(function(){
+			$(this).find(".gmyy").css('display',"block");
+			$(this).addClass("te");
+			$(this).siblings().removeClass("te");
+			$(this).siblings().find(".fkyy , .zxyp ,.guke, .pifu , .changwei ,.wuguan ").css("display","none");
+		})
+		$("main .caw").eq(m).find(" .title .title-r a").eq(3).hover(function(){
+			$(this).find(".guke").css('display',"block");
+			$(this).addClass("te");
+			$(this).siblings().removeClass("te");
+			$(this).siblings().find(".fkyy , .gmyy ,.zxyp, .pifu , .changwei ,.wuguan ").css("display","none");
+		})
+		$("main .caw").eq(m).find(" .title .title-r a").eq(4).hover(function(){
+			$(this).find(".pifu").css('display',"block");
+			$(this).addClass("te");
+			$(this).siblings().removeClass("te");
+			$(this).siblings().find(".fkyy , .gmyy ,.guke, .zxyp , .changwei ,.wuguan ").css("display","none");
+		})
+
+		$("main .caw").eq(m).find(" .title .title-r a").eq(5).hover(function(){
+			$(this).find(".changwei").css('display',"block");
+			$(this).addClass("te");
+			$(this).siblings().removeClass("te");
+			$(this).siblings().find(".fkyy , .gmyy ,.guke, .pifu , .zxyp ,.wuguan ").css("display","none");
+		})
+		$("main .caw").eq(m).find(" .title .title-r a").eq(6).hover(function(){
+			$(this).find(".wuguan").css('display',"block");
+			$(this).addClass("te");
+			$(this).siblings().removeClass("te");
+			$(this).siblings().find(".fkyy , .gmyy ,.guke, .pifu , .changwei ,.zxyp ").css("display","none");
+		})
+
+}
+
+$("#LoutiNav ul li").hover(function() {
+				//鼠标滑上去
+				$(this).addClass("hover");
+			}, function() {
+				//鼠标移开
+				$(this).removeClass("hover");
+			});
+			 //鼠标点击
+			var mark = 1;
+			$("#LoutiNav ul li").click(function() {
+				mark = 2; //改变标记
+				$("#LoutiNav ul li").find("span").removeClass("active");
+				$(this).find("span").addClass("active");
+				//点击左边导航 然后跳到指定的楼层
+				var $index = $(this).index(); //找到了对应的序列号
+				//alert($index);
+				var $top = $("main .Louti").eq($index).offset().top; //获取制定Louti与浏览器上面的距离
+				//alert($top);
+				$("body,html").animate({
+					scrollTop: $top
+				}, 500, function() {
+					mark = 1;
+				}); //浏览器滚动的高度
+			});
+			 //浏览器窗口滚动事件
+		
+
+			  $(window).scroll(function() {
+				if (mark == 1) {
+					var $t = $(this).scrollTop(); //获取滚动条滚动的高度
+					//document.title = $t;
+					if ($t > 1000) { //通过滚动条来判断
+						$("#LoutiNav").fadeIn(); //淡入 导航慢慢显示出来
+					} else {
+						$("#LoutiNav").fadeOut(); //淡出 导航慢慢消失
+					}
+					var $obj = $("#main .Louti");
+					//循环每一个Louti 然后找到最先满足条件的那个 Louti
+					$obj.each(function() {
+						var $index = $(this).index();
+						//楼层与浏览器上面的高度
+						var $height = $obj.eq($index).offset().top + $(this).height() / 2;
+						//alert($height) 
+						//document.title = $t + "--" + $height;
+						if ($t < $height) {
+							$("#LoutiNav ul li").find("span").removeClass("active")
+							$("#LoutiNav ul li").eq($index).find("span").addClass("active");
+							return false;
+						}
+					});
+				}
+				
+               if($(this).scrollTop()>=1796&&$(this).scrollTop()<2400){
+               	      $("#LoutiNav ul li").find("span").removeClass("active");
+				      $("#LoutiNav ul li").find("span").eq(0).addClass("active");    
+               }else{
+          	         $("#LoutiNav ul li").find("span").eq(0).removeClass("active");
+				     
+               }
+              if($(this).scrollTop()>=2400&&$(this).scrollTop()<2900){
+               	      $("#LoutiNav ul li").find("span").removeClass("active");
+				      $("#LoutiNav ul li").find("span").eq(1).addClass("active");    
+               }else{
+          	         $("#LoutiNav ul li").find("span").eq(1).removeClass("active");
+				     
+               }
+               if($(this).scrollTop()>=2900&&$(this).scrollTop()<3467){
+               	      $("#LoutiNav ul li").find("span").removeClass("active");
+				      $("#LoutiNav ul li").find("span").eq(2).addClass("active");    
+               }else{
+          	         $("#LoutiNav ul li").find("span").eq(2).removeClass("active");
+				     
+               }
+               if($(this).scrollTop()>=3467&&$(this).scrollTop()<4198){
+               	      $("#LoutiNav ul li").find("span").removeClass("active");
+				      $("#LoutiNav ul li").find("span").eq(3).addClass("active");    
+               }else{
+          	         $("#LoutiNav ul li").find("span").eq(3).removeClass("active");
+				     
+               }
+               if($(this).scrollTop()>=4198&&$(this).scrollTop()<4788){
+               	      $("#LoutiNav ul li").find("span").removeClass("active");
+				      $("#LoutiNav ul li").find("span").eq(4).addClass("active");    
+               }else{
+          	         $("#LoutiNav ul li").find("span").eq(4).removeClass("active");
+				     
+               }
+               if($(this).scrollTop()>=4788&&$(this).scrollTop()<=5368){
+               	      $("#LoutiNav ul li").find("span").removeClass("active");
+				      $("#LoutiNav ul li").find("span").eq(5).addClass("active");    
+               }else{
+          	         $("#LoutiNav ul li").find("span").eq(5).removeClass("active");
+				     
+               }
+			
+			});
+$("#hanging a").eq(0).hover(function(){
+	
+	           $(this).find(".xin").css("display","block");
+},function(){
+               $(this).find(".xin").css("display","none");
+})
+			
+$("#hanging a").eq(2).hover(function(){
+	
+	           $(this).find(".xiaowei").css("display","block");
+},function(){
+               $(this).find(".xiaowei").css("display","none");
+})
+$("#hanging a").eq(3).click(function(){
+	
+	           $("html,body").animate({"scrollTop": '0px'}, 1000);
+})		
